79e884528476be3f65d2449761e359c02f046991
Dmitry Kudriavtsev <dkudriavtsev@gmail.com>
2019-11-30 21:08:38 -0800

    Day 1 - Ruby

---
+++ ruby/days/01/part2.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+
+# AoC 2019 Day 2
+
+f = File.read 'input.txt'
+
+main_sum = 0
+
+f.lines.each do |l|
+  sum = l.to_i / 3 - 2
+  new_req = sum
+
+  until (new_req / 3 - 2) <= 0
+    new_req = new_req / 3 - 2
+    sum += new_req
+    puts new_req
+  end
+
+  main_sum += sum
+end
+
+
+
+puts main_sum
